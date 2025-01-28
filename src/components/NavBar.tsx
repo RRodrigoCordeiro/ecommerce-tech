0660df028acd553d5d8376a78f497d25236c71c1
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-01-27 21:02:34 -0300

    ✨ feat implementação do menu dropdown

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdMenu } from "react-icons/io";
 import { MdOutlineQuestionAnswer } from "react-icons/md";
 import { FaPeopleGroup } from "react-icons/fa6";
 import { BsCart2 } from "react-icons/bs";
 
 const NavBar = () => {
+    const [dropdownVisible, setDropdownVisible]= useState<number | null>(null);
+
+    const produtos = [
+      {
+        nome: "Produto 1",
+        opcoes: [
+          {nome:"Detalhes", link:"/produto-a/detalhes", imagem: ""},
+          {nome:"Compra", link:"/produto-a/compra", imagem: ""},
+          {nome: "comparar", link:"/produto-a/comparar", imagem: ""}
+        ],
+      },
+      {
+        nome: "Produto 2",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 3",
+        opcoes: [
+          { nome: "Configurações", link: "/produto-c/configuracoes", imagem: "" },
+          { nome: "Promoções", link: "/produto-c/promocoes", imagem: "" },
+          { nome: "Personalizar", link: "/produto-c/personalizar", imagem: "" },
+        ],
+      },
+      {
+        nome: "Produto 4",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 5",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 6",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 7",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho",imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 8",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 9",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+      {
+        nome: "Produto 10",
+        opcoes: [
+          {nome: "Ver estoque", link: "/produto-b/estoque", imagem: ""},
+          {nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: ""},
+          {nome: "Avaliações", link: "/produto-b/carrinho", imagem: ""},
+        ],
+      },
+    ]
+
   return (
     <div>
       <div className='flex flex-row items-center mb-16 mt-11 justify-evenly'>
@@ -39,28 +124,45 @@ const NavBar = () => {
 
         </div>
       </div>
-
       <div className="bg-green-500 w-full flex justify-center text-white font-bold p-4 gap-7">
+         {produtos.map((produto, index) => (
+            <div
+              key={index}
+              className="relative"
+              onMouseEnter={() => setDropdownVisible(index)}
+              onMouseLeave={() => setDropdownVisible(null)}
+            >
+              <div className='flex flex-row gap-8'>
+                <p className='cursor-pointer'>{produto.nome}</p>
+                <span className="border-l-2 border-white h-6"></span>
+              </div>
 
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
-          <span className="border-l-2 border-white h-6"></span>
-          <p>Produto X</p>
+              {dropdownVisible === index && (
+                <div className="fixed top-64 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-black p-4 shadow-lg rounded w-[1400px] z-50">
+                  <ul className=" flex flex-row items-center justify-center space-x-10">
+                      {produto.opcoes.map((opcao, idx) => (
+                          <li
+                            key={idx}
+                          >
+                            <a
+                              key={opcao.link}
+                              
+                              className="hover:text-green-500 cursor-pointer block"
+                            >
+                              <img 
+                                src={opcao.imagem} 
+                                // alt={opcao.nome} 
+                                className="w-12 h-12 object-cover mr-2"
+                              />
+                              {opcao.nome}
+                            </a>
+                          </li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+         ))}
       </div>
     </div>
   );
