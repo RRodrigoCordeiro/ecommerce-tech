174ac8162d526360b3f52a997c0ad998f6676511
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-02-06 22:33:38 -0300

    ♻️ refactor: Refatoração no código

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import React from 'react'
+import { useEffect, useState } from "react"
+import axios from "axios"
 import Header from './components/Header'
 import Carrossel from './components/Carrossel'
-import ElectronicsList from './components/ElectronicsList'
+
+
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/category/electronics")
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+        setLoading(false);
+      });
+  },[])
+
+  if(loading) return <p>Carregando...</p>
+  if (error) return <p>Ocorreu um erro: {error.message}</p>
+
+  
   return (
     <div>
       <Header/>
       <Carrossel/>
-      <ElectronicsList/>
+      <div>
+        <ul>
+          {products.map((product)=> (
+            <li
+              key={product.id}
+            >
+              <h2>{product.title}</h2>
+              <img
+                src={product.image}
+                alt={product.title}
+                width="100"
+              >
+              </img>
+              <p>{product.descriptoon}</p>
+              <p>Preço: R$ {product.price}</p>
+            </li>
+          ))}
+          <li>
+
+          </li>
+        </ul>
+      </div>
+     
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import { IoMdMenu } from "react-icons/io";
+import { MdOutlineQuestionAnswer } from "react-icons/md";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { IoPerson } from "react-icons/io5";
+import { BsCart2 } from "react-icons/bs";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoMdMic } from "react-icons/io";
+
+const Header = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(null);
+
+  const produtos = [
+    {
+      nome: "Produto 1",
+      opcoes: [
+        { nome: "Detalhes", link: "/produto-a/detalhes", imagem: "" },
+        { nome: "Compra", link: "/produto-a/compra", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    {
+      nome: "Produto 2",
+      opcoes: [
+        { nome: " estoque", link: "/produto-b/estoque", imagem: "" },
+        { nome: " estoque ", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    {
+      nome: "Produto 3",
+      opcoes: [
+        { nome: "Configurações", link: "/produto-c/configuracoes", imagem: "" },
+        { nome: "Promoções", link: "/produto-c/promocoes", imagem: "" },
+        { nome: "Personalizar", link: "/produto-c/personalizar", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    {
+      nome: "Produto 4",
+      opcoes: [
+        { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+        { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    {
+      nome: "Produto 5",
+      opcoes: [
+        { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+        { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    {
+      nome: "Produto 6",
+      opcoes: [
+        { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+        { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+        { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+      ],
+    },
+    // Caso queira reativar os produtos 7, 8, 9 e 10, basta descomentar as linhas abaixo:
+    // {
+    //   nome: "Produto 7",
+    //   opcoes: [
+    //     { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+    //     { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+    //   ],
+    // },
+    // {
+    //   nome: "Produto 8",
+    //   opcoes: [
+    //     { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+    //     { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+    //   ],
+    // },
+    // {
+    //   nome: "Produto 9",
+    //   opcoes: [
+    //     { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+    //     { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+    //   ],
+    // },
+    // {
+    //   nome: "Produto 10",
+    //   opcoes: [
+    //     { nome: "Ver estoque", link: "/produto-b/estoque", imagem: "" },
+    //     { nome: "ver estoque 2", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "Avaliações", link: "/produto-b/carrinho", imagem: "" },
+    //     { nome: "comparar", link: "/produto-a/comparar", imagem: "" },
+    //   ],
+    // },
+  ];
+
+  return (
+    <div>
+      <div className="flex flex-col bg-green-600 items-center mb-16 justify-evenly md:bg-white md:flex-row md:mt-11">
+        <div className="flex flex-row justify-around items-center w-full mt-3 mb-3 md:mt-0 md:mb-0 md:w-0">
+          <IoMdMenu size={40} className="md:hidden" />
+          <p>LOGO</p>
+        </div>
+
+        <div className="relative w-80 md:ml-8 lg:w-96">
+          <input
+            type="text"
+            className="border border-gray-300 rounded px-4 py-4 w-80 md:ml-8 lg:w-96"
+            placeholder="Buscar"
+          />
+          <IoSearchOutline className="absolute right-8 top-1/2 transform -translate-y-1/2" />
+          <IoMdMic className="absolute right-1 top-1/2 transform -translate-y-1/2" />
+        </div>
+
+        <div className="flex flex-row gap-10 mt-8 mb-3 md:mt-0 md:mb-0 md:gap-5 lg:gap-12">
+          <div className="flex flex-col items-center">
+            <MdOutlineQuestionAnswer size={28} />
+            <p>Sobre nós</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <FaPeopleGroup size={28} />
+            <p>SAC</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <BsCart2 size={28} />
+            <p>Carrinho</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <IoPerson size={28} />
+            <p>Login</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-green-500 w-full hidden justify-center text-white font-bold p-4 gap-7 md:flex">
+        {produtos.map((produto, index) => (
+          <div
+            key={index}
+            className="relative"
+            onMouseEnter={() => setDropdownVisible(index)}
+            onMouseLeave={() => setDropdownVisible(null)}
+          >
+            <div className="flex flex-row gap-5">
+              <p className="cursor-pointer">{produto.nome}</p>
+              <span className="border-l-2 border-white h-6"></span>
+            </div>
+
+            {dropdownVisible === index && (
+              <div className="fixed top-64 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-black p-4 shadow-lg rounded md:w-[750px] lg:w-[1270px] z-50">
+                <ul className="flex flex-row items-center justify-center space-x-10">
+                  {produto.opcoes.map((opcao, idx) => (
+                    <li key={idx}>
+                      <a className="hover:text-green-500 cursor-pointer block">
+                        <img
+                          src={opcao.imagem}
+                          // alt={opcao.nome}
+                          className="w-12 h-12 object-cover mr-2"
+                        />
+                        {opcao.nome}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
