2419f2d5186aa140639b5fad2469df5fe9a0f3d6
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-02-17 10:59:55 -0300

    ✨ feat: Implementação da funcionalidade de accordion no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,111 +1,235 @@
-import React from "react";
+import { useState } from "react";
 
 const Footer = () => {
-  return (
-    <div className="flex justify-center md:gap-9  lg:gap-28 mt-28 p-20  bg-[#D9D9D9]">
-      <div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = (index) => {
+    setIsOpen(isOpen === index ? null : index);
+  };
+
+  const items = [
+    {
+      title: "Atendimento",
+      content: (
         <div>
-          <p className="font-bold text-blue-600 text-sm lg:text-lg ">Mídias Sociais</p>
-          <div className="mb-8 mt-8 space-y-2  grid grid-cols-2 ">
-            <img 
+          <p>Horários:</p>
+          <ul>
+            <li>Segunda à Sexta - 08:00 às 20:00</li>
+            <li>Sábado e Domingo - 09:00 às 15:00 (Exceto feriados)</li>
+          </ul>
+        </div>
+      ),
+    },
+    { title: "SAC", content: "Clique aqui" },
+    {
+      title: "Institucional",
+      content: (
+        <ul>
+          <li>Segurança & Privacidade</li>
+          <li>Políticas de Privacidade</li>
+          <li>Código de Defesa do Consumidor</li>
+          <li>Sobre nós</li>
+          <li>Trabalhe conosco</li>
+        </ul>
+      ),
+    },
+    { title: "Acessibilidade", content: "Acessibilidade" },
+    {
+      title: "Pedidos e compras",
+      content: "Envio, entrega e coleta; Devoluções e trocas; Contate-nos",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="hidden md:flex justify-center md:gap-9 lg:gap-28 mt-28 p-20 bg-[#D9D9D9]">
+        <div>
+          <div>
+            <p className="font-bold text-blue-600 text-sm lg:text-lg ">
+              Mídias Sociais
+            </p>
+            <div className="mb-8 mt-8 space-y-2  grid grid-cols-2 ">
+              <img
                 src="/images/LogoInstagram.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            <img 
+              />
+              <img
                 src="/images/LogoFacebook.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            <img 
+              />
+              <img
                 src="/images/LogoTwitter.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            <img 
+              />
+              <img
                 src="/images/LogoYoutuber.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            <img 
+              />
+              <img
                 src="/images/Logolinkedin.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            <img 
+              />
+              <img
                 src="/images/LogoInstagram.png"
                 alt="Logo do instagram"
                 className="w-10 "
-            />
-            {/* <p>LOGO FACEBOOK</p>
-            <p>LOGO INSTAGRAM</p>
-            <p>LOGO TWITTER</p>
-            <p>LOGO YOUTUBER</p>
-            <p>LOGO LINKEDLIN</p>
-            <p>LOGO TIKTOK</p> */}
+              />
+            </div>
+          </div>
+
+          <div>
+            <p className="font-bold text-blue-600  lg:text-lg">
+              Baixe os aplicativos
+            </p>
+            <div className="mb-8 mt-8 space-y-8">
+              <img
+                src="/images/GooglePlay.png"
+                alt="Imagem da logo do Google Play"
+              />
+              <img
+                src="/images/AppStore.png"
+                alt="Imagem da logo do Google Play"
+              />
+            </div>
           </div>
         </div>
 
         <div>
-          <p className="font-bold text-blue-600  lg:text-lg">Baixe os aplicativos</p>
-          <div className="mb-8 mt-8 space-y-8">
+          <div>
+            <p className="font-bold text-blue-600 lg:text-lg">Atendimento</p>
+            <div className="mb-8 mt-8 space-y-2">
+              <p>Horário de Atendimento - (Exceto feriados)</p>
+              <p className="max-w-36">
+                <span className="font-semibold">Segunda a Sexta:</span> 08:00 às
+                20:00
+              </p>
+              <p className="max-w-37">
+                <span className="font-semibold">Sábado e Domingo:</span> 09:00
+                às 15:00
+              </p>
+              <p className="font-semibold">Endereço:</p>
+              <p className="max-w-52 -mt-2">
+                Rua Branco Cabo,123 - Cabo Branco, cep: 12345-67
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <p className="font-bold text-blue-600 lg:text-lg">SAC</p>
+            <p className="mt-8 mb-8">Clique aqui</p>
+          </div>
+          <div>
+            <p className="font-bold text-blue-600 text-lg mt-36">
+              Institucional
+            </p>
+            <div className="mb-8 mt-8 space-y-2">
+              <p>Segurança & Privacidade</p>
+              <p>Políticas de Privacidade</p>
+              <p>Código de Defesa do Consumidor</p>
+              <p>Sobre Nós</p>
+              <p>Trabalhe Conosco</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <p className="font-bold text-blue-600 lg:text-lg">Acessibilidade</p>
+            <p className="mb-8 mt-8">Acessibilidade</p>
+          </div>
+          <div>
+            <p className="font-bold text-blue-600 text-lg mt-36">
+              Pedidos e Compras
+            </p>
+            <div className="mb-8 mt-8 space-y-2">
+              <p>Envio, entrega e coleta</p>
+              <p>Devoluções e trocas</p>
+              <p>Contate-nos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:hidden justify-center md:gap-9 lg:gap-28 mt-28 p-20 bg-[#D9D9D9]">
+        <div className="max-w-lg space-y-4">
+          {items.map((item, index) => (
+            <div key={index} className="border border-gray-300 rounded-lg">
+              <button
+                className="w-full p-4 text-left flex justify-between items-center bg-gray-100 hover:bg-gray-200 gap-12"
+                onClick={() => toggleAccordion(index)}
+              >
+                <span>{item.title}</span>
+                <span>{isOpen === index ? "-" : "+"}</span>
+              </button>
+              {isOpen === index && (
+                <div className="p-4 text-center bg-white border-t">
+                  {item.content}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div>
+          <h1 className="text-blue-600 text-center mt-12 font-bold text-lg">
+            Midias Sociais
+          </h1>
+          <div className="mb-8 mt-8 space-y-2 grid grid-cols-3 justify-items-center items-center ">
+            <img
+              src="/images/LogoInstagram.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+            <img
+              src="/images/LogoFacebook.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+            <img
+              src="/images/LogoTwitter.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+            <img
+              src="/images/LogoYoutuber.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+            <img
+              src="/images/Logolinkedin.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+            <img
+              src="/images/LogoInstagram.png"
+              alt="Logo do instagram"
+              className="w-10 "
+            />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-blue-600 text-center mt-12 font-bold text-lg">
+            Baixe os aplicativos
+          </h1>
+          <div className="mb-8 mt-8 flex flex-col items-center justify-center gap-8 ">
             <img
               src="/images/GooglePlay.png"
-              alt="Imagem da logo do Google Play"
+              alt="Logo do instagram"
+              className="w-48"
             />
             <img
               src="/images/AppStore.png"
-              alt="Imagem da logo do Google Play"
+              alt="Logo do instagram"
+              className="w-48 "
             />
           </div>
         </div>
-      </div>
-
-      <div>
-        <div>
-            <p className="font-bold text-blue-600 lg:text-lg">Atendimento</p>
-            <div className="mb-8 mt-8 space-y-2">
-                <p>Horário de Atendimento - (Exceto feriados)</p>
-                <p className="max-w-36"><span className="font-semibold">Segunda a Sexta:</span> 08:00 às 20:00</p>
-                <p className="max-w-37"><span className="font-semibold">Sábado e Domingo:</span> 09:00 às 15:00</p>
-                <p className="font-semibold">Endereço:</p>
-                <p className="max-w-52 -mt-2">Rua Branco Cabo,123 - Cabo Branco, cep: 12345-67</p>
-            </div>
-        </div>
-      </div>
-
-      <div>
-         <div>
-            <p className="font-bold text-blue-600 lg:text-lg">SAC</p>
-            <p className="mt-8 mb-8">Clique aqui</p>
-         </div>
-         <div>
-            <p className="font-bold text-blue-600 text-lg mt-36">Institucional</p>
-            <div className="mb-8 mt-8 space-y-2" >
-                <p>Segurança & Privacidade</p>
-                <p>Políticas de Privacidade</p>
-                <p>Código de Defesa do Consumidor</p>
-                <p>Sobre Nós</p>
-                <p>Trabalhe Conosco</p>
-
-            </div>
-         </div>
-      </div>
-
-      <div>
-        <div>
-            <p className="font-bold text-blue-600 lg:text-lg">Acessibilidade</p>
-            <p className="mb-8 mt-8">Acessibilidade</p>
-        </div>
-        <div>
-            <p className="font-bold text-blue-600 text-lg mt-36">Pedidos e Compras</p>
-            <div className="mb-8 mt-8 space-y-2">
-                <p>Envio, entrega e coleta</p>
-                <p>Devoluções e trocas</p>
-                <p>Contate-nos</p>
-            </div>
-        </div>
-
       </div>
     </div>
   );
