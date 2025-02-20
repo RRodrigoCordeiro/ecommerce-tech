3c7c4696d481d9510e89bb726a5a97eb9096edd2
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-02-19 23:18:16 -0300

    ✨ feat: Criação da página de SAC

---
+++ src/pages/Sac.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Sac = () => {
+  return (
+    <div>
+       <Header/>    
+    Sac
+       <Footer/>
+    </div>
+  )
+}
+
+export default Sac
