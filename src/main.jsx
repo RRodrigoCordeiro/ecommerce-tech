3b72b5e55a330040d78b260170261edd42416e50
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-03-12 23:05:04 -0300

    ✨ feat: download do react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { register } from 'swiper/element/bundle'
 
+
 register();
+
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -13,8 +16,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-fade'
 
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 )
